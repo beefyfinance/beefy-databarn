99bbe82133aea5b703e8b471bde8b607a42525d4
prevostc <998369+prevostc@users.noreply.github.com>
2026-02-18 10:40:41 +0100

    Add connection test

---
+++ infra/clickhouse/demo_connect.py
@@ -0,0 +1,22 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#   "argparse",
+#   "clickhouse-connect",
+# ]
+# ///
+
+import clickhouse_connect
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--host', type=str, required=True)
+parser.add_argument('--port', type=int, required=True)
+parser.add_argument('--username', type=str, required=True)
+parser.add_argument('--password', type=str, required=True)
+args = parser.parse_args()
+
+client = clickhouse_connect.get_client(host=args.host, port=args.port, username=args.username, password=args.password)
+
+result = client.query('SELECT 1')
+print(result.result_rows)
